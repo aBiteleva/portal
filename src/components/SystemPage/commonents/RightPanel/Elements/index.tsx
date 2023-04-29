071348bd77e4b65b4c0f3587613366f1f1046c4f
anastasiabiteleva <a.biteleva@setupit.org>
2023-04-29 15:08:09 +0300

    промежуточный

--- src/components/SystemPage/commonents/RightPanel/Elements/index.tsx
+++ src/components/SystemPage/commonents/RightPanel/Elements/index.tsx
@@ -1,32 +1,40 @@
 import Icon from '../../../../../common/components/Icon';
 import React from 'react';
 import stylesCommon from '../../../../../common/styles/styles.module.scss';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import variables from '../../../../../../variables.module.scss';
 
 const Elements = () => {
-    return <>
-        <div className={stylesCommon.rightPanelBlockTitle}>Элементы системы</div>
-        <div className={stylesCommon.rightPanelBlockAction}>
-            <Icon name="events"/>
-            <div className={stylesCommon.rightPanelBlockActionText}>
-                {/*eslint-disable-next-line*/}
+  return (
+    <>
+      <div className={stylesCommon.rightPanelBlockTitle}>Элементы системы</div>
+      <div className={stylesCommon.rightPanelBlockAction}>
+        <Icon name="events" />
+        <div className={stylesCommon.rightPanelBlockActionText}>
+          {/*eslint-disable-next-line*/}
 
-                <Link style={{textDecoration: 'none', color: variables.yellowColor}} to="/events">
-                    Список событий
-                </Link>
-            </div>
+          <Link
+            style={{ textDecoration: 'none', color: variables.yellowColor }}
+            to="/events"
+          >
+            Список событий
+          </Link>
         </div>
-        <div className={stylesCommon.rightPanelBlockAction}>
-            <Icon name="study"/>
-            <div className={stylesCommon.rightPanelBlockActionText}>
-                {/*eslint-disable-next-line*/}
-                <Link style={{textDecoration: 'none', color: variables.yellowColor}} to="/devices">
-                    Список правил
-                </Link>
-            </div>
+      </div>
+      <div className={stylesCommon.rightPanelBlockAction}>
+        <Icon name="study" />
+        <div className={stylesCommon.rightPanelBlockActionText}>
+          {/*eslint-disable-next-line*/}
+          <Link
+            style={{ textDecoration: 'none', color: variables.yellowColor }}
+            to="/devices"
+          >
+            Список правил
+          </Link>
         </div>
-    </>;
+      </div>
+    </>
+  );
 };
 
 export default Elements;
