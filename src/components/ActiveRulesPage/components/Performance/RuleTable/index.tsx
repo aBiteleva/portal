d1f2e09e901c2541050aa69ff51f0e8e1b246fef
anastasiabiteleva <a.biteleva@setupit.org>
2023-05-06 15:05:17 +0300

    добавлено табличное представление

--- src/components/ActiveRulesPage/components/Performance/RuleTable/index.tsx
+++ src/components/ActiveRulesPage/components/Performance/RuleTable/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import commonStyles from '../../../../../common/styles/styles.module.scss';
+import {Table} from "antd";
+import {columns} from "./resources/columns";
 import {useTypedSelector} from "../../../../../hooks/useTypedSelector";
 
 const RuleTable = () => {
+    const {activeRules} = useTypedSelector(state => state.activeRulesValues);
 
     return <>
         <div className={commonStyles.elementsContainer}>
-            <Table dataSource={dataSource} columns={columns} />;
+            <Table dataSource={activeRules} columns={columns} pagination={false}/>
         </div>
     </>;
 };
