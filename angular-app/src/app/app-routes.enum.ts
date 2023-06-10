1ee351a03d126711c96b7f2ffbfea01b58375c8d
k.beloshapko <k.beloshapko001@gmail.com>
2023-06-10 21:25:33 +0300

    add route 'editor-active-rules'

--- angular-app/src/app/app-routes.enum.ts
+++ angular-app/src/app/app-routes.enum.ts
@@ -1,5 +1,6 @@
 export enum AppRoutesEnum {
     EVENTS = 'events',
+    EDITOR_ACTIVE_RULES = 'editor-active-rules',
     ACTIVE_RULE_SCRIPT = 'active-rules-script',
     EVENT_SCRIPT = 'events-script',
     CREATE = 'new',
